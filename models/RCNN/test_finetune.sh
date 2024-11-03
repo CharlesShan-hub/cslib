5d569d9d2b5bb593fa04ce0ecb9269a53ca3bedb
CharlesShan-hub <charles.shht@gmail.com>
2024-11-03 19:02:35 +0800

    Update test_finetune.sh

--- models/RCNN/test_finetune.sh
+++ models/RCNN/test_finetune.sh
@@ -13,11 +13,11 @@ fi
 
 PYTHON_SCRIPT="test_finetune.py"
 RES_PATH="${BASE_PATH}/Model/RCNN/Flowers17"
-
+# --model_path "${RES_PATH}/AlexNet_FineTune/checkpoints/53.pt" \
 # pth on the Mac PC
 python $PYTHON_SCRIPT \
     --comment "(RCNN on 2flowers) step2: Finetune AlexNet Classifier(with pre-trained model)" \
-    --model_path "${RES_PATH}/AlexNet_FineTune/checkpoints/53.pt" \
+    --model_path "${RES_PATH}/2024_11_03_10_53/checkpoints/8.pt" \
     --dataset_path "${BASE_PATH}/torchvision"\
     --num_classes 3 \
     --image_size 224 \
