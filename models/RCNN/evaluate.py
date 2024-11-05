b7c977dd7b2be48f54d8dad6e3c1292fd3bf96a2
CharlesShan-hub <charles.shht@gmail.com>
2024-11-05 14:06:19 +0800

    修正了一下结果

--- models/RCNN/evaluate.py
+++ models/RCNN/evaluate.py
@@ -51,6 +51,7 @@ def test():
     # Selected Search
     _, regions = selective_search(image, scale=500, sigma=0.9, min_size=10)
     images,labels,rects = crop_and_filter_regions(image, INPUT_KEY, regions, INPUT_GT, 224, 0.5)
+    # glance(images,shape=(2,29),title=labels)
 
     # Get Features
     images_tensor = torch.stack([to_tensor(i) for i in images])
@@ -60,30 +61,33 @@ def test():
     results = []
     results_old = []
     results_label = []
-    
+    flower = {1:'pancy', 2:'Tulip'}
+
     for i in range(len(images)):
         for svm in [svm1_model,svm2_model]:
             feature = features[i,:].data.cpu().numpy()
             pred = svm.predict([feature.tolist()])
             if pred[0] == 0:
                 continue
-            box = reg_model(features[i,:])
-            if box[0]<0.3:
+            if rects[i][0] == 0:
                 continue
-            # print(box)
-            box = box.data.cpu().numpy()
-            px, py, pw, ph = rects[i][0], rects[i][1], rects[i][2], rects[i][3]
-            old_center_x, old_center_y = px + pw / 2.0, py + ph / 2.0
-            x_ping, y_ping, w_suo, h_suo = box[1], box[2], box[3], box[4],
-            new__center_x = x_ping * pw + old_center_x
-            new__center_y = y_ping * ph + old_center_y
-            new_w = pw * np.exp(w_suo)
-            new_h = ph * np.exp(h_suo)
-            new_verts = [new__center_x, new__center_y, new_w, new_h]
-            results.append(new_verts)
+            box = reg_model(features[i,:]).data.cpu().numpy()
+            # if box[0]<0.3:
+            if box[0]<0.8:
+                continue
+            (_, bx, by, bw, bh) = box # Eg. box [0.9368, 0.1490, 0.1342, 0.9999, 1.0000]
+            (_, px, py, pw, ph) = rects[i] # Eg. rects[i] [1, 60, 15, 403, 368]
+            new_x = px + pw*bx
+            new_y = py + ph*by
+            new_w = bw*pw
+            new_h = bh*ph
+            results.append([new_x, new_y , new_w, new_h])
             results_label.append(pred[0])
+            # print(box)
+            # print(rects[i])
+            # print([new_x+new_w/2, new_y+new_h/2, new_w, new_h])
+            # show_rect(image, [rects[i][1:]], flower[pred[0]])
 
-    flower = {1:'pancy', 2:'Tulip'}
     average_center_x, average_center_y, average_w,average_h = 0, 0, 0, 0
     #use average values to represent the final result
     for vert in results:
@@ -98,8 +102,9 @@ def test():
     average_result = [[average_center_x, average_center_y, average_w, average_h]]
     result_label = max(results_label, key=results_label.count)
     print(result_label,average_result)
-    # show_rect(image, results_old, ' ')
+    # show_rect(image, results, ' ')
     show_rect(image, average_result, flower[result_label])
+    # show_rect(image, [INPUT_GT], flower[result_label])
 
 if __name__ == "__main__":
     test()
